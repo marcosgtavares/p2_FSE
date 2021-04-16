c7cc45f3e0d4b694c2bb4e70f54690bf486dfffe
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-04-16 09:17:32 -0400

    comand line input

--- central/src/main.c
+++ central/src/main.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "../inc/server.h"
 #include "../inc/client.h"
 
 int main(int argc, char *argv[]) {
 
-    unsigned short gate = 5001;
+    unsigned short gate = atoi(argv[1]);;
     char ip[10] = "127.0.1.1";
     char message2[16] = "urdur";
 
--- distribuido/src/main.c
+++ distribuido/src/main.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "../inc/server.h"
 #include "../inc/client.h"
 
 int main(int argc, char *argv[]) {
-    unsigned short serv = 5001;
+    unsigned short serv = atoi(argv[1]);;
 
     int servsocket=open_socket(serv);
 
