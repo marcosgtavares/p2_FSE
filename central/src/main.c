54af6127754af2b4ac00003599faa15e1d1b8ed7
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-04-16 09:09:23 -0400

    ip change

--- central/src/main.c
+++ central/src/main.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
 
     unsigned short gate = 4001;
-    char ip[12] = "192.168.0.8";
+    char ip[10] = "127.0.1.1";
     char message2[16] = "urdur";
 
     int cliente = connect_server(gate, ip);
