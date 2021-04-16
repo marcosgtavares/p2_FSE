bb3e85ed2830f77979cf465f12602606288a2712
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-04-16 08:16:18 -0400

    server and client functions added

---
+++ central/src/client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int connect_server(unsigned short servidorPorta, char *IP_Servidor){
+    int clienteSocket;
+    struct sockaddr_in servidorAddr;
+    // Criar Socket
+	if((clienteSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		printf("Erro no socket()\n");
+    // Construir struct sockaddr_in
+	memset(&servidorAddr, 0, sizeof(servidorAddr)); // Zerando a estrutura de dados
+	servidorAddr.sin_family = AF_INET;
+	servidorAddr.sin_addr.s_addr = inet_addr(IP_Servidor);
+	servidorAddr.sin_port = htons(servidorPorta);
+    if(connect(clienteSocket, (struct sockaddr *) &servidorAddr, 
+							sizeof(servidorAddr)) < 0)
+		printf("Erro no connect()\n");
+    
+    return clienteSocket;
+}
+
+void send_message(char *mensagem, int clienteSocket){
+    char buffer[16];
+    int tamanhoMensagem = strlen(mensagem);
+    int totalBytesRecebidos;
+    int bytesRecebidos;
+
+    if(send(clienteSocket, mensagem, tamanhoMensagem, 0) != tamanhoMensagem)
+		printf("Erro no envio: numero de bytes enviados diferente do esperado\n");
+
+	totalBytesRecebidos = 0;
+	while(totalBytesRecebidos < tamanhoMensagem) {
+		if((bytesRecebidos = recv(clienteSocket, buffer, 16-1, 0)) <= 0)
+			printf("Não recebeu o total de bytes enviados\n");
+		totalBytesRecebidos += bytesRecebidos;
+		buffer[bytesRecebidos] = '\0';
+		printf("%s\n", buffer);
+	}
+	close(clienteSocket);
+    //close(clienteSocket);
+
+}
