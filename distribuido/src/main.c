2bd973a77943236802d8355f7d1cd08f0935d7c1
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-04-17 12:38:54 -0400

    join

--- distribuido/src/main.c
+++ distribuido/src/main.c
@@ -38,6 +38,8 @@ int main(int argc, char *argv[]) {
 
     //pthread_create(&m_tr, NULL, treat_messages, (void *)&servsocket ); // thread it
 
+    printf("%d", (void *)&servsocket);
+
     treat_messages((void *)&servsocket);
     
     
