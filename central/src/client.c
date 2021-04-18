1e4976ff8826ee4fbe2fd6228ebc9cd154596a96
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-04-17 23:56:37 -0400

    correction

--- central/src/client.c
+++ central/src/client.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include "../inc/client.h"
 #include "../inc/control_acess.h"
--- central/src/main.c
+++ central/src/main.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+
 #include <pthread.h> 
 //#include <signal.h>
 
